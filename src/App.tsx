904ed55b2e48cfcf7ecbef97e7a0e9627bc56167
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2023-08-15 11:42:07 -0300

    chore: Configurando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
-export function App() {  
-
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button variant='success'/>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
       <Button />
 
       <GlobalStyle />
