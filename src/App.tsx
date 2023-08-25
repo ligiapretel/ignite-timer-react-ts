3897de9de7e5381f2f7ac818f1fe0e2ae92ab768
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2023-08-24 21:12:49 -0300

    delete: Excluindo arquivo de exemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-// import { ThemeProvider } from 'styled-components'
-// import { BrowserRouter } from 'react-router-dom'
-// import { Router } from './Router'
-// import { defaultTheme } from './styles/themes/default'
-// import { GlobalStyle } from './styles/global'
-import { ExampleContextAPI } from './ExampleContextAPI'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <ExampleContextAPI />
-    // <ThemeProvider theme={defaultTheme}>
-    //   <BrowserRouter>
-    //     <Router />
-    //   </BrowserRouter>
-    //   <GlobalStyle />
-    // </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
